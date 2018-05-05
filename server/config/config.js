88516bd1fefd0572dde2ac25f4c137768be83317
Gerardo Cetzal <gerardoce23@gmail.com>
2018-05-05 01:30:07 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://gcetzalb:Alvsecret0@ds215910.mlab.com:15910/cafe-udemy';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
